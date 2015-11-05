e0325a2f9e00799bc8e4392baec65aa2d4750892
sanztss <sanztss@gmail.com>
2015-11-05 02:13:06 -0200

    Update DrMundo.lua

--- DrMundo.lua
+++ DrMundo.lua
@@ -1,5 +1,8 @@
 require('Inspired')
-MundoMenu = Menu("Mundo", "Mundo")
+
+if GetObjectName(GetMyHero()) ~= "DrMundo" then return end
+
+MundoMenu = Menu("DrMundo", "DrMundo")
 MundoMenu:SubMenu("Combo", "Combo")
 MundoMenu.Combo:Boolean("Q", "Use Q", true)
 MundoMenu.Combo:Boolean("W", "Use W", true)
@@ -7,55 +10,60 @@ MundoMenu.Combo:Boolean("E", "Use E", true)
 MundoMenu.Combo:Boolean("R", "Use R", true)
 
 OnTick(function(myHero)
+
 	local target  = GetCurrentTarget()
+
 	if IOW:Mode() == "Combo" then
 
-		
 		local QPred   = GetPredictionForPlayer(myHeroPos(),target,GetMoveSpeed(target),2000,250,1050,75,true,false)
 		local WRange  = 325
-		local WbRange = 355 
+		local WbRange = 805
 		local wUsed   = false
 
-
+		-- AUTO CAST Q
 		if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 and ValidTarget(target, 1050) and GetDistance(myHero, target) <= 999 and MundoMenu.Combo.Q:Value() then
 			CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 		end
 
-
-		if GetDistance(myHero, target) < WRange and MundoMenu.Combo.W:Value() then
-                if wUsed == false then
-                        CastSkillShot(_W)
-                        wUsed = true
-                end
-        elseif GetDistance(myHero, target) > wbRange and wUsed == true and MundoMenu.Combo.W:Value() then
-                CastSkillShot(_W)
-                wUsed = false
-        end
-
-
-		if CanUseSpell(myHero, _E) == READY and ValidTarget(target, 225) and GetDistance(myHero, target) <= 200 and MundoMenu.Combo.E:Value() then
-			CastSkillShot(_E)
+		-- AUTO CAST W
+		if GotBuff(myHero, "BurningAgony") ~= 1 then
+			if CanUseSpell(myHero, _W) == READY and ValidTarget(target, 325) then
+				CastTargetSpell(myHero, _W)
+			end
 		end
 
-	end
-
-	if myHero.health < (myHero.maxHealth*(20*0.01)) then
-		if CanUseSpell(myHero, _R) then
-			CastSkillShot(_R)
+		if GotBuff(myHero, "BurningAgony") == 1 then
+			if CanUseSpell(myHero, _W) == READY and GetDistance(myHero, target) >= 600 or ValidTarget(target, 325) == nil then
+				CastTargetSpell(myHero, _W)
+			end
 		end
+
+		-- AUTO CAST E
+		if CanUseSpell(myHero, _E) == READY and ValidTarget(target, 325) and GetDistance(myHero, target) <= 200 and MundoMenu.Combo.E:Value() then
+			CastSpell(_E)
+		end
+
+		-- AUTO CAST R
+		local minhaHP = GetCurrentHP(myHero)
+		if  minhaHP < (GetMaxHP(myHero)*(20*0.01)) then
+			if CanUseSpell(myHero, _R) then
+				CastSpell(_R)
+			end
+		end
+
 	end
 
 end)
+
 OnDraw(function(myHero)
 
-for i,enemy in pairs(GetEnemyHeroes()) do
+	for i,enemy in pairs(GetEnemyHeroes()) do
+		if ValidTarget(enemy, 2000) then
+			local trueDMG = getdmg("Q",target)
+			local currhp = GetCurrentHP(enemy) - trueDMG
+			DrawDmgOverHpBar(enemy,currhp,3,trueDMG,0xff00ffff)
+		end
 
-  if ValidTarget(enemy, 2000) then
-  local trueDMG = getdmg("Q",target,myHero,GetCastLevel(source, _Q))
-  DrawDmgOverHpBar(enemy,GetCurrentHP(enemy),trueDMG,0,0xffffff00)
-  end
-    
-end
+	end
 
 end)
-
